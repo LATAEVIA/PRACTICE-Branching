4ea4bfb778f6a6d1d489c4afbfa275b74a2d9e24
LATAEVIA <lataevia.berry@gmail.com>
2016-03-29 14:47:38 -0700

    add completed animals folder

--- AnimalInfo/js/scripts.js
+++ AnimalInfo/js/scripts.js
@@ -11,12 +11,12 @@
 //   }
 // });
 
-$(document).ready(function() {
-  var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
-
-  if (over21) {
-    $('#drinks').show();
-  } else {
-    $('#under-21').show();
-  }
-});
+// $(document).ready(function() {
+//   var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+//
+//   if (over21) {
+//     $('#drinks').show();
+//   } else {
+//     $('#under-21').show();
+//   }
+// });
