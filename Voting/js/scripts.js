c734de29ec62613ca78156af6b8c7314205a950b
LATAEVIA <lataevia.berry@gmail.com>
2016-03-29 13:28:14 -0700

    add completed vote practice folder with nested if else

--- Voting/js/scripts.js
+++ Voting/js/scripts.js
@@ -12,11 +12,21 @@
 // });
 
 $(document).ready(function() {
-  var over21 = confirm("Are you over 21? Click OK for yes or Cancel for no.");
+  var over18 = parseInt(prompt("How old are you?"));
 
-  if (over21) {
-    $('#drinks').show();
+  if (over18 >= 18) {
+    $('#voter').show();
+
+  } else if (over18 === 17) {
+    var justInTime = prompt('Will you be 18 in time for polling day? Yes or No');
+
+    if (justInTime === "yes" || justInTime === "Yes" || justInTime === "YES") {
+      $('#voter').show();
+
+    } else {
+      $('#minor').show();
+    }
   } else {
-    $('#under-21').show();
+      $('#minor').show();
   }
 });
